457965a6aa18236d9e6be250e0fbbbadfa883e86
Anisse Astier <anisse@astier.eu>
2026-07-06 07:16:32 +0200

    PoC with genanki-rs-rev fork that has wasm support

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use genanki_rs_rev::{Deck, Error, Note, Package, basic_model};
 use log::info;
 use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::wasm_bindgen;
@@ -5,6 +6,41 @@ use wasm_bindgen::prelude::wasm_bindgen;
 #[wasm_bindgen]
 pub fn generate_set() {
     info!("Hello, world!");
+
+    let mut deck = Deck::new(1234, "Example Deck", "Example Deck containing 2 Flashcards");
+    deck.add_note(
+        Note::new(
+            basic_model(),
+            vec!["What is the capital of France?", "Paris"],
+        )
+        .unwrap(),
+    );
+    deck.add_note(
+        Note::new(
+            basic_model(),
+            vec!["What is the capital of Germany?", "Berlin"],
+        )
+        .unwrap(),
+    );
+
+    let package = Package::new(vec![deck], std::collections::HashMap::new()).unwrap();
+    package.write(NullWriter {}).unwrap();
+}
+
+struct NullWriter {}
+impl std::io::Write for NullWriter {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
+impl std::io::Seek for NullWriter {
+    fn seek(&mut self, _: std::io::SeekFrom) -> std::io::Result<u64> {
+        Ok(0)
+    }
 }
 
 #[wasm_bindgen(start)]
